5336cafe8b4c2e8ae854ca38dea0aaba9f17b4a8
Sergei Borja <serge.borja0107@gmail.com>
2025-02-25 09:11:25 -0700

    Adjusted loading screen component, style and file locations.

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,37 +1,32 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import Section from '@/components/Layout/Section';
 import PageHeader from '@/components/Layout/PageHeader';
 import Card from '@/components/Card';
-import LoadingScreen from '../../components/LoadingScreen';
+import LoadingScreen from '@/components/LoadingScreen';
 
 function Dashboard() {
     const [isLoading, setIsLoading] = useState(true);
-    
-    // Fake loading screen cuz who gotta know?
 
+    // Fake loading screen cuz who gotta know?
     useEffect(() => {
-      
         const hasLoadedOnce = sessionStorage.getItem('dashboardLoadedOnce');
-    
+
         if (hasLoadedOnce) {
-          setIsLoading(false);
-        } else {
-          // Show loading screen once per login session
-          const timer = setTimeout(() => {
             setIsLoading(false);
-            sessionStorage.setItem('dashboardLoadedOnce', 'true');
-          }, 5000); // Adjust the delay as needed
-    
-          return () => clearTimeout(timer);
+        } else {
+            // Show loading screen once per login session
+            const timer = setTimeout(() => {
+                setIsLoading(false);
+                sessionStorage.setItem('dashboardLoadedOnce', 'true');
+            }, 2000);
+
+            return () => clearTimeout(timer);
         }
-      }, []);
-    
+    }, []);
+
 
     if (isLoading) {
-        return (
-            <LoadingScreen variant="full-screen">
-            </LoadingScreen>
-        );
+        return (<LoadingScreen/>);
     }
 
     return (
