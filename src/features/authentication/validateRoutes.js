f552b8c809858fefed1d1b1c03a4a472be951352
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2025-03-04 14:31:26 -0700

    created endpoints for validation testing

---
+++ src/features/authentication/validateRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { validateUserCreateDTO, validateUserLogInDTO, validateEmailDTO } = require('../controllers/validationController');
+
+router.post('/validate-register', validateUserCreateDTO);
+router.post('/validate-login', validateUserLogInDTO);
+router.post('/validate-email', validateEmailDTO);
+
+module.exports = router;
